1f62bb8c6a0b60bd658c718f835e0599ffc80eea
davidHuyDang <davidhuydang02@gmail.com>
2025-01-22 16:58:40 -0500

    Update IsPurchasable.swift

--- Group10_Gym/Protocols/IsPurchasable.swift
+++ Group10_Gym/Protocols/IsPurchasable.swift
@@ -1,3 +1,9 @@
+//
+//  IsPurchasable.swift
+//  Group10_Gym
+//
+//  Created by LLH on 2025-01-16.
+//
 import Foundation
 
 // Protocol defining that a class must be "purchasable", meaning it should have some service information and be able to print a receipt.
